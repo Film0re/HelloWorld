b3b57d38aecea915c00f78e57af1ab9e826c43b3
Daniel <dandoodle956@gmail.com>
2021-03-01 17:15:15 -0600

    Added comments

--- src/com/company/HelloWorld.java
+++ src/com/company/HelloWorld.java
@@ -3,6 +3,7 @@ package com.company;
 public class HelloWorld {
 
     public static void main(String[] args) {
+        //prints out Hello World
         System.out.println("Hello World");
     }
 }
